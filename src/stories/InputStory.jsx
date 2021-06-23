603f30522b261ee367730f694917b056f6d40224
Jonghyuk Lee <jhlee@symyoo.com>
2021-06-23 17:32:07 +0900

    스토리북 세팅 완료

--- src/stories/InputStory.jsx
+++ src/stories/InputStory.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
 import Input from "../03/Input";
 
 storiesOf("Input", module)
-  .add("기본설정", () => <Input name="name" />)
-  .add("label 예제", () => <Input name="name" label="이름" />);
+  .addWithJSX("기본설정", () => <Input name="name" />)
+  .addWithJSX("label 예제", () => <Input name="name" label="이름" />)
+  .addWithJSX("OnChange 예제", () => (
+    <Input name="name" onChange={action("OnChange 이벤트 발생")} />
+  ));

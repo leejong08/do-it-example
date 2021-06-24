032aec64044ec1f2e49b1b31eb979b10251157cd
Jonghyuk Lee <jhlee@symyoo.com>
2021-06-24 17:28:51 +0900

    머터리얼 파일 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import Input from "./03/Input";
+import "./App.css";
+import "./materialize.css";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Input />
+        <nav>
+          <div className="nav-wrapper">
+            <div>두잇리액트 시작하기</div>
+          </div>
+        </nav>
+        <h1>머티리얼</h1>
       </div>
     );
   }
